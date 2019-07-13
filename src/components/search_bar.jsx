71431ed8135a4308291e935c19e6c91afcb68dfe
MICHEL Dany <donnetonfalzard+gitlab@gmail.com>
2019-07-14 01:55:07 +0200

    color searchbar when mouse over event

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -3,14 +3,27 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {colored: false};
     }
     handleChange = (event) => {
         this.props.searchFunction(event.target.value);
     }
+    handleMouseOver = (event) => {
+        this.setState({colored: true});
+    }
+    handleMouseOut = (event) => {
+        this.setState({colored: false});
+    }
+    inputClasses() {
+        let result = "form-control form-search";
+        if (this.state.colored) {
+            result += " colored";
+        } 
+        return result;
+    }
     render() {
         return (
-            <input type="text" className="form-control form-search" onChange={this.handleChange}/>
+            <input type="text" className={this.inputClasses()} onChange={this.handleChange} onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}/>
         )
     }
 }
